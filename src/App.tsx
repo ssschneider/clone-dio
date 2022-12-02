8982f8cac104a1feb04fe58b2f67bbb1396f4ac1
Sarah Schneider <mureb_rosa@hotmail.com>
2022-12-01 22:44:01 -0300

    create signup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Cadastro } from "./pages/cadastro/cadastro";
 import Feed from "./pages/feed/feed";
 import HomePage from "./pages/home/home"
 import Login from "./pages/login/login"
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/feed" element={<Feed />}/>
+        <Route path="/cadastro" element={<Cadastro />}/>
       </Routes>
     </BrowserRouter>
   );
